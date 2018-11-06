9a6ea58cd4328bf07a35bd8161605e84a3372b1a
Cody Conroy <cdac77@gmail.com>
2018-11-05 22:25:03 -0500

    Indoor Tri Image Gallery and Nav Changes

---
+++ angular-firebase/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageDetailComponent } from './image-detail/image-detail.component';
+import { UploadComponent } from './upload/upload.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GalleryComponent,
+    ImageDetailComponent,
+    UploadComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
